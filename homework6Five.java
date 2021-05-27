75f350e69ede3647abb1eb0dfedb0562fc7a1919
coderFeedForwardAlg <76785692+coderFeedForwardAlg@users.noreply.github.com>
2021-05-27 11:07:45 -0400

    changed name to be more accurate

--- homework6Five.java
+++ homework6Five.java
@@ -4,7 +4,7 @@ Max Scott
 this is a school project for numerical analysis, homewrok 6 part 5 
 ***the partitiond trapozoidal method for finding intargals*** 
 */
-public class homework6Five {
+public class trapezoidal method {
     public static void main(String[] args) {
         double a = 1.0;
         double b = 1.5;
