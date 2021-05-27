73239304786626bca20b74af44249a8586a12743
coderFeedForwardAlg <76785692+coderFeedForwardAlg@users.noreply.github.com>
2021-05-27 11:06:32 -0400

    this is a iterative numerical integration program

---
+++ homework6Five.java
@@ -0,0 +1,30 @@
+/*
+Max Scott
+2020-2021 school year
+this is a school project for numerical analysis, homewrok 6 part 5 
+***the partitiond trapozoidal method for finding intargals*** 
+*/
+public class homework6Five {
+    public static void main(String[] args) {
+        double a = 1.0;
+        double b = 1.5;
+        double n = 4;  
+        double h = (b-a)/n;
+        double computTrap = f(a);
+        for(int i =1; i < n; i++){
+            double x = a + i*h;
+            computTrap += 2*f(x);
+        }
+        computTrap += f(b);
+        
+        computTrap = computTrap*h/2;
+        System.out.println("the aria is: " + computTrap);
+    }
+
+    public static double f(double x){
+        return Math.pow(x,2)*Math.log(x);
+
+    }
+    
+    
+}
